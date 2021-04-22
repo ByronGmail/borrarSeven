9f319eed9577ad60e37239fd0453e89389a7cb3d
ByronGmail <83036489+ByronGmail@users.noreply.github.com>
2021-04-22 14:48:27 -0500

    sugerido

--- pull.php
+++ pull.php
@@ -6,3 +6,9 @@
     <li><a href="#">Contacto</a></li>
   </ol>
 </nav>
+  <ol>
+    <li><a href="#">Home</a></li>
+    <li><a href="#">Quienes somos</a></li>
+    <li><a href="#">Nosotros</a></li>
+    <li><a href="#">Contacto</a></li>
+  </ol>
